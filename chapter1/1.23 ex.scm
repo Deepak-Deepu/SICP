053d128079c756769dfbf86fa4230b1421b0f9ae
deepu <deepakcsgec@gmail.com>
2016-05-02 10:15:18 +0530

    1.22 ex, 1.23 ex

---
+++ chapter1/1.23 ex.scm
@@ -0,0 +1,41 @@
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+
+(define (next test-divisor)
+(if (= test-divisor 2) 
+3
+(+ test-divisor 2)))
+
+(define (timed-prime-test n range count)
+  (start-prime-test n (runtime) range count))
+
+(define (start-prime-test n start-time range count)
+  (if (prime? n) 
+      (cond ((< n range) (report-prime n (- (runtime) start-time) range count)))
+      (timed-prime-test (+ n 1) range count)))
+
+(define (report-prime n elapsed-time range count)
+  (newline)
+  (display n)
+  (display " *** ")
+  (display elapsed-time)
+  (if (< count 3) 
+  (timed-prime-test (+ n 1) range (+ count 1))
+  (display "over")))
+
+
+(timed-prime-test 1000 1050 1)
