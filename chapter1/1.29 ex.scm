40e678aa9debc83af6741461542c675ea5d48cf0
deepu <deepakcsgec@gmail.com>
2016-05-03 20:04:02 +0530

    1.29 1.30 added

---
+++ chapter1/1.29 ex.scm
@@ -0,0 +1,27 @@
+(define (sum term a next b)
+(if (> a b)
+0
+(+ (term a)
+(sum term (next a) next b))))
+
+
+(define (cube x)
+(* x x x))
+
+(define (simpson f a b n)
+(define  h (/ (- b a) n))
+(define (yk k)
+(f (+ a (* k h))))
+(define (term k)
+(* (cond (( = 0 k) 1)
+         (( = n k) 1)
+         ((even? k) 2)
+          (else 4))
+          (yk k)))
+
+(define (inc x)
+(+ 1 x))
+(/ (* h (sum term 0 inc n)) 3))
+
+(simpson cube 0 1 100.0)
+; .24999999999999992
